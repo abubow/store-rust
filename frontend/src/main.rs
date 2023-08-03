8dce2b58ac851103d7e7a49e2c45110aa8af340d
Syed Mohammad Abuzar <sabuzar00@gmail.com>
2023-08-03 14:44:43 +0500

    basic hello setup yew

--- frontend/src/main.rs
+++ frontend/src/main.rs
@@ -1,3 +1,10 @@
+use yew::prelude::*;
+#[function_component]
+fn App() -> Html{
+    html!{
+        <h1>{"hello"}</h1>
+    }
+}
 fn main() {
-    println!("Hello, world!");
+    yew::Renderer::<App>::new().render();
 }
